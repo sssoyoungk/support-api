cfc08ac0840f8e5c08f66155fb8e4f1de613e7f1
ach1230 <ach1230@labgenomics.com>
2022-11-11 13:58:33 +0900

    배포판 api목록 안보이게 수정

--- supportsapi.labgenomics.com/App_Start/RouteConfig.cs
+++ supportsapi.labgenomics.com/App_Start/RouteConfig.cs
@@ -12,11 +12,16 @@ namespace supportsapi.labgenomics.com
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+#if DEBUG
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+#else
+                routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}"                
+#endif
             );
         }
     }
