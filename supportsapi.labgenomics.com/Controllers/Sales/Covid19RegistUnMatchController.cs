9b9721eb141000237e0564becfd5fce5c538df02
DESKTOP-0POVOG9\lee <mhlee@labgenomics.com>
2022-12-13 11:22:55 +0900

    질청 미매치 리스트 및 질청 중복 확인 리스트 Get 추가, DB프로젝트 추가 정보 관리 API 추가

---
+++ supportsapi.labgenomics.com/Controllers/Sales/Covid19RegistUnMatchController.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using supportsapi.labgenomics.com.Attributes;
+using supportsapi.labgenomics.com.Services;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+
+namespace supportsapi.labgenomics.com.Controllers.Sales
+{
+    [SupportsAuth]
+    [Route("api/Sales/RegistUnMatch")]
+    public class Covid19RegistUnMatchController : ApiController
+    {
+
+        /// <summary>
+        /// 미매치 리스트 
+        /// </summary>
+        /// <param name="beginDate">시작일</param>
+        /// <param name="endDate">종료일</param>
+        /// <param name="authGroupCode">그룹 코드</param>
+        /// <returns></returns>
+        public IHttpActionResult Get(DateTime beginDate, DateTime endDate, string authGroupCode)
+        {
+
+            StringBuilder query = new StringBuilder();
+            query.Append("select *\n");
+            query.Append("from\n");
+            query.Append("(select a.LabRegDate, a.LabRegNo, a.CompCode, b.CompName\n");
+            query.Append(", case when d.TestDisplayName like '%그룹%' then e.CustomValue01 else a.PatientName end as PatientName\n");
+            query.Append(", case when d.TestDisplayName like '%그룹%' then dbo.FUNC_GetIdxDataLikeSplit(e.CustomValue02, 1, ';') else a.PatientJuminNo01 + '-' + master.dbo.AES_DecryptFunc(a.PatientJuminNo02, 'labge$%#!dleorms') end as PatientJuminNo\n");
+            query.Append(", case when d.TestDisplayName like '%그룹%' then dbo.FUNC_GetIdxDataLikeSplit(e.CustomValue02, 2, ';') else a.SystemUniqID end as SampleNo\n");
+            query.Append(", c.OrderCode, d.TestDisplayName\n");
+            query.Append(", a.IsTrustOrder, a.CenterCode\n");
+            query.Append("from LabRegInfo as a\n");
+            query.Append("inner join ProgCompCode as b on a.CompCode = b.CompCode\n");
+            query.Append("inner join LabRegOrder as c on a.LabRegDate = c.LabRegDate and a.LabRegNo = c.LabRegNo\n");
+            query.Append("inner join LabTestCode as d on c.OrderCode = d.TestCode\n");
+            query.Append("left outer join LabRegCustom as e on a.LabRegDate = e.LabRegDate and a.LabRegNo = e.LabRegNo and e.CustomCode in ('5401', '5402', '5403', '5404', '5405')\n");
+            query.Append($"where a.LabRegDate between '{beginDate.ToString("yyyy-MM-dd")}' and '{endDate.ToString("yyyy-MM-dd")}'\n");
+            query.Append("and b.CompDemandGroupCode = '24'\n");
+            query.Append("--and isnull(a.CenterCode, '') = 'Covid19Excel'\n");
+            query.Append(") as r\n");
+            query.Append($"where r.CompCode in (select CompCode from ProgAuthGroupAccessComp where AuthGroupCode = '{authGroupCode}')\n");
+            query.Append("and r.SampleNo != ''\n");
+            query.Append("and r.SampleNo not like 'SC%'\n");
+            query.Append("and r.SampleNo not like 'SA%'\n");
+            query.Append("and r.SampleNo not like 'TI%'\n");
+            query.Append("and r.SampleNo not like 'KP%'\n");
+            query.Append("and r.SampleNo not like 'IR%'\n");
+            query.Append("order by r.LabRegDate, r.LabRegNo\n");
+
+            var arrResponse = LabgeDatabase.SqlToJArray(query.ToString());
+
+            return Ok(arrResponse);
+        }
+
+    }
+}
---
+++ supportsapi.labgenomics.com/Controllers/StrategyBusiness/ManageDBProjectInfoController.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using supportsapi.labgenomics.com.Attributes;
+using supportsapi.labgenomics.com.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
+{
+    [SupportsAuth]
+    [Route("api/StrategyBusiness/ManageDBProject")]
+    public class ManageDBProjectInfoController : ApiController
+    {
+        public IHttpActionResult Get(DateTime beginDate, DateTime endDate)
+        {
+#if DEBUG
+            string infoTable = "PGSPatientInfo_Test";
+
+#else
+            string infoTable = "PGSPatientInfo";
+#endif
+
+            string sql;
+            sql =
+                $"SELECT\r\n" +
+                $"    ppi.CompOrderDate, ppi.CompOrderNo, ppi.Gender, ppi.Race, ppi.BirthDay, ppi.PatientName, ppi.ZipCode, ppi.Address, ppi.Address2, ppi.EmailAddress, \r\n" +
+                $"    ppi.PhoneNumber, ppi.AgreePrivacyPolicyDateTime, ppi.AgreeGeneTest, ppi.AgreeThirdPartyOffer, ppi.PrevTrakingNumber, ppi.PrevBarcode, ppi.TrakingNumber, ppi.ReshippedCode , ppi.AgreeThirdPartySensitive, ppi.Barcode, \r\n" +
+                $"    ppi.AgreeGeneThirdPartySensitive, ppi.AgreeKeepDataAndFutureAnalysis, ppi.OrderStatus, CONVERT(varchar, ltcoi.LabRegDate, 23) AS LabRegDate, ltcoi.LabRegNo\r\n" +
+                $"FROM {infoTable} ppi\r\n" +
+                $"LEFT OUTER JOIN LabTransCompOrderInfo ltcoi\r\n" +
+                $"ON ltcoi.CompOrderDate = ppi.CompOrderDate\r\n" +
+                $"AND ltcoi.CompOrderNo = ppi.CompOrderNo\r\n" +
+                $"AND ltcoi.CompCode = ppi.CompCode\r\n" +
+                $"WHERE ppi.CustomerCode = 'GenoCore'\r\n" +
+                $"AND ppi.CompOrderDate BETWEEN '{beginDate.ToString("yyyy-MM-dd")}' AND '{endDate.ToString("yyyy-MM-dd")}'\r\n";
+
+            JArray arrResponse = LabgeDatabase.SqlToJArray(sql);
+            return Ok(arrResponse);
+        }
+
+
+
+
+        public IHttpActionResult Put([FromBody]JObject objRequest)
+        {
+            //업데이트
+
+            return Ok();
+        }
+
+
+
+
+        public IHttpActionResult Delete(string LabRegDate, string LabRegNo)
+        {
+            return Ok();
+        }
+
+
+    }
+}
