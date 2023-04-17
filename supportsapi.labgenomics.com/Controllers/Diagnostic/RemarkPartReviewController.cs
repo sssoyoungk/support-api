4bedde1887fe68b916d80018366b1fd287cf611a
이민호 <mhlee@labgenomics.com>
2023-04-17 09:28:21 +0900

    GFRupdate, GlucoseStat, RemarkpartReview, ResultPivot  생성 및 method 소스 추가

---
+++ supportsapi.labgenomics.com/Controllers/Diagnostic/RemarkPartReviewController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using supportsapi.labgenomics.com.Attributes;
+using supportsapi.labgenomics.com.Services;
+using System;
+using System.Net;
+using System.Web.Http;
+
+namespace supportsapi.labgenomics.com.Controllers.Diagnostic
+{
+    public class RemarkPartReviewController : ApiController
+    {
+
+        [Route("api/Diagnostic/RemarkPartReview/PartCode")]
+        public IHttpActionResult Get()
+        {
+            string sql;
+            sql = "select PartCode as 코드, PartName as 파트명 from LabPartCode where PartSeqNo between '10' and '100' order by PartSeqNo";
+            var arrResponse = LabgeDatabase.SqlToJArray(sql);
+            return Ok(arrResponse);
+        }
+        
+
+
+
+
+    }
+}
