01e9224c0f29097a1592835882316b48643bd38b
ach1230 <ach1230@labgenomics.com>
2023-05-12 15:18:22 +0900

    쿼리 수정

--- supportsapi.labgenomics.com/Controllers/Sales/CompRegistOrder/AmorePacific.cs
+++ supportsapi.labgenomics.com/Controllers/Sales/CompRegistOrder/AmorePacific.cs
@@ -45,7 +45,8 @@ namespace supportsapi.labgenomics.com.Controllers.Sales.CompRegistOrder
                         $"AND ltmc.CompMatchCode = pti.CompTestCode\r\n" +
                         $"LEFT OUTER JOIN LabTestCode ltc\r\n" +
                         $"ON ltmc.CenterMatchCode = ltc.TestCode\r\n" +
-                        $"WHERE ppi.CompOrderDate >= '{request["BeginDate"]}'\r\n" +
+                        $"WHERE ppi.CustomerCode = 'amorepacific'\r\n" +
+                        $"AND ppi.CompOrderDate >= '{request["BeginDate"]}'\r\n" +
                         $"AND ppi.CompOrderDate < DATEADD(DAY, 1, '{request["EndDate"]}')\r\n" +
                         $"AND ppi.CompCode = '{request["CompCode"]}'\r\n" +
                         $"AND NOT EXISTS\r\n" +
@@ -57,6 +58,38 @@ namespace supportsapi.labgenomics.com.Controllers.Sales.CompRegistOrder
                         $"    AND orderInfo.CompTestCode = pti.CompTestCode\r\n" +
                         $")\r\n" +
                         $"ORDER BY CompOrderDate, CompOrderNo";
+
+
+                    //sql =
+                    //    $"SELECT\r\n" +
+                    //    $"    CONVERT(bit,0) AS ColumnCheck, GETDATE() AS LabRegDate, null AS LabRegNo, appi.PatientName, appi.Barcode AS PatientChartNo\r\n" +
+                    //    $"  , appi.CompOrderDate, appi.CompOrderNo, appi.Barcode, CONVERT(varchar, appi.SampleDrawDate, 120) SampleDrawDate\r\n" +
+                    //    $"  , CONVERT(CHAR(6), BirthDay, 12) AS IdentificationNo1, Gender AS PatientSex\r\n" +
+                    //    $"  , FLOOR(CAST(DATEDIFF(DAY, appi.BirthDay, appi.CompOrderDate) AS INTEGER)/365.2422) AS PatientAge\r\n" +
+                    //    $"  , ltmc.CenterMatchCode AS TestCode, ltmc.CenterMatchOrderCode AS OrderCode\r\n" +
+                    //    $"  , appi.TestCode AS CompTestCode, null AS CompTestSubCode, appi.TestName\r\n" +
+                    //    $"  , CASE WHEN ISNULL(ltmc.CenterMatchSampleCode, '') <> '' THEN ltmc.CenterMatchSampleCode\r\n" +
+                    //    $"         ELSE ltc.SampleCode END AS SampleCode\r\n" +
+                    //    $"FROM AmorePacificPatientInfo appi\r\n" +
+                    //    $"JOIN ProgCompCode pcc\r\n" +
+                    //    $"ON appi.CompCode = pcc.CompCode\r\n" +
+                    //    $"LEFT OUTER JOIN LabTransMatchCode AS ltmc\r\n" +
+                    //    $"ON ltmc.CompCode = appi.CompCode\r\n" +
+                    //    $"AND ltmc.CompMatchCode = appi.TestCode\r\n" +
+                    //    $"LEFT OUTER JOIN LabTestCode ltc\r\n" +
+                    //    $"ON ltmc.CenterMatchCode = ltc.TestCode\r\n" +
+                    //    $"WHERE appi.CompOrderDate >= '{request["BeginDate"]}'\r\n" +
+                    //    $"AND appi.CompOrderDate < DATEADD(DAY, 1, '{request["EndDate"]}')\r\n" +
+                    //    $"AND appi.CompCode = '{request["CompCode"]}'\r\n" +
+                    //    $"AND NOT EXISTS\r\n" +
+                    //    $"(\r\n" +
+                    //    $"    SELECT NULL FROM LabTransCompOrderInfo orderInfo\r\n" +
+                    //    $"    WHERE orderInfo.CompCode = appi.CompCode\r\n" +
+                    //    $"    AND orderinfo.CompOrderDate = appi.CompOrderDate\r\n" +
+                    //    $"    AND orderinfo.CompOrderNo = appi.CompOrderNo\r\n" +
+                    //    $"    AND orderInfo.CompTestCode = appi.TestCode\r\n" +
+                    //    $")\r\n" +
+                    //    $"ORDER BY CompOrderDate, CompOrderNo\r\n";                    
                 }
                 else
                 {
