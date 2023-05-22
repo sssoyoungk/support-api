f8549b589f038a557da3ed07eafb1b38cea4faa6
ach1230 <ach1230@labgenomics.com>
2023-05-22 14:23:11 +0900

    소스 정리

--- supportsapi.labgenomics.com/Controllers/StrategyBusiness/ManageBankSaladController.cs
+++ supportsapi.labgenomics.com/Controllers/StrategyBusiness/ManageBankSaladController.cs
@@ -20,7 +20,7 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
                 subQuery = $"AND pi2.CompTestCode = '{testCode}' \r\n";
             }
 
-            if (mode == "Ordered") 
+            if (mode == "Ordered")
             {
                 sql =
                     $"SELECT pi2.CompTestCode, pi2.CompTestName, \r\n" +
@@ -38,7 +38,7 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
                     $"AND ltcoi.CompCode = ppi.CompCode\r\n" +
                     $"WHERE ppi.CustomerCode = 'banksalad'\r\n" +
                     $"{subQuery}" +
-                    $"AND ppi.CompOrderDate BETWEEN '{beginDate.ToString("yyyy-MM-dd")}' AND '{endDate.ToString("yyyy-MM-dd")}'\r\n" +
+                    $"AND ppi.CompOrderDate BETWEEN '{beginDate:yyyy-MM-dd}' AND '{endDate:yyyy-MM-dd}'\r\n" +
                     $"AND (ppi.Server <> 'Develop' or ppi.Server is null)\r\n" +
                     $"AND ppi.OrderStatus = 'Ordered'";
             }
@@ -52,9 +52,9 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
                     $"    ppi.AgreeGeneThirdPartySensitive, ppi.AgreeKeepDataAndFutureAnalysis, ppi.OrderStatus, CONVERT(varchar, ltcoi.LabRegDate, 23) AS LabRegDate, ltcoi.LabRegNo, CONVERT(varchar(19), lrr.ReportTransEndTime, 21) AS ReportTransEndTime, CONVERT(varchar(19), lrr.ReportEndTime, 21) AS ReportEndTime, ISNULL(lrr.IsReportTransEnd, 0) as IsReportTransEnd \r\n" +
                     $"FROM PGSPatientInfo ppi\r\n" +
                     $"join PGSTestInfo pi2\n" +
-                    $"on pi2.CompOrderDate  = ppi.CompOrderDate\n"+
-                    $"and pi2.CompOrderNo  = ppi.CompOrderNo\n"+
-                    $"and pi2.CustomerCode = ppi.CustomerCode\n"+
+                    $"on pi2.CompOrderDate  = ppi.CompOrderDate\n" +
+                    $"and pi2.CompOrderNo  = ppi.CompOrderNo\n" +
+                    $"and pi2.CustomerCode = ppi.CustomerCode\n" +
                     $"LEFT OUTER JOIN LabTransCompOrderInfo ltcoi\r\n" +
                     $"ON ltcoi.CompOrderDate = ppi.CompOrderDate\r\n" +
                     $"AND ltcoi.CompOrderNo = ppi.CompOrderNo\r\n" +
@@ -64,7 +64,7 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
                     $"AND lrr.LabRegNo  = ltcoi.LabRegNo\n" +
                     $"WHERE ppi.CustomerCode = 'banksalad'\r\n" +
                     $"{subQuery}" +
-                    $"AND ppi.CompOrderDate BETWEEN '{beginDate.ToString("yyyy-MM-dd")}' AND '{endDate.ToString("yyyy-MM-dd")}'\r\n" +
+                    $"AND ppi.CompOrderDate BETWEEN '{beginDate:yyyy-MM-dd}' AND '{endDate:yyyy-MM-dd}'\r\n" +
                     $"AND (ppi.Server <> 'Develop' or ppi.Server is null)\r\n" +
                     $"AND ppi.OrderStatus != 'Ordered'\r\n" +
                     $"order by ppi.CompOrderDate ";
@@ -87,7 +87,7 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
                     $"AND ltcoi.CompCode = ppi.CompCode\r\n" +
                     $"WHERE ppi.CustomerCode = 'banksalad'\r\n" +
                     $"{subQuery}" +
-                    $"AND ppi.CompOrderDate BETWEEN '{beginDate.ToString("yyyy-MM-dd")}' AND '{endDate.ToString("yyyy-MM-dd")}'\r\n" +
+                    $"AND ppi.CompOrderDate BETWEEN '{beginDate:yyyy-MM-dd}' AND '{endDate:yyyy-MM-dd}'\r\n" +
                     $"AND (ppi.Server <> 'Develop' or ppi.Server is null)\r\n" +
                     $"AND ppi.OrderStatus in('Returned', 'Reshipped') AND ppi.PrevBarcode != '' \r\n" +
                     $"AND ppi.CheckSendCollectReSample = '0' " +
@@ -124,23 +124,23 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
             }
 
 
-                sql =
-                    $"SELECT pti.CompTestCode, pti.CompTestName, \r\n" +
-                    $"    ppi.CompOrderDate, ppi.CompOrderNo, ppi.BirthDay, ppi.PatientName,  ppi.CheckSendCollectReSample, ppi.EmailAddress, \r\n" +
-                    $"    ppi.PhoneNumber,  ppi.PrevBarcode, ppi.PrevTrackingNumber, ppi.TrackingNumber, ppi.Barcode, \r\n" +
-                    $"    ppi.OrderStatus, CONVERT(varchar, ltcoi.LabRegDate, 23) AS LabRegDate, ltcoi.LabRegNo\r\n" +
-                    $"FROM PGSPatientInfo ppi\r\n" +
-                    $"join PGSTestInfo pti\n" +
-                    $"on pti.CompOrderDate  = ppi.CompOrderDate\n" +
-                    $"and pti.CompOrderNo  = ppi.CompOrderNo\n" +
-                    $"and pti.CustomerCode = ppi.CustomerCode\n" +
-                    $"LEFT OUTER JOIN LabTransCompOrderInfo ltcoi\r\n" +
-                    $"ON ltcoi.CompOrderDate = ppi.CompOrderDate\r\n" +
-                    $"AND ltcoi.CompOrderNo = ppi.CompOrderNo\r\n" +
-                    $"AND ltcoi.CompCode = ppi.CompCode\r\n" +
-                    $"WHERE ppi.CustomerCode = 'banksalad'\r\n" +
-                    $"{subQuery}" +
-                    $"order by ppi.CompOrderDate ";
+            sql =
+                $"SELECT pti.CompTestCode, pti.CompTestName, \r\n" +
+                $"    ppi.CompOrderDate, ppi.CompOrderNo, ppi.BirthDay, ppi.PatientName,  ppi.CheckSendCollectReSample, ppi.EmailAddress, \r\n" +
+                $"    ppi.PhoneNumber,  ppi.PrevBarcode, ppi.PrevTrackingNumber, ppi.TrackingNumber, ppi.Barcode, \r\n" +
+                $"    ppi.OrderStatus, CONVERT(varchar, ltcoi.LabRegDate, 23) AS LabRegDate, ltcoi.LabRegNo\r\n" +
+                $"FROM PGSPatientInfo ppi\r\n" +
+                $"join PGSTestInfo pti\n" +
+                $"on pti.CompOrderDate  = ppi.CompOrderDate\n" +
+                $"and pti.CompOrderNo  = ppi.CompOrderNo\n" +
+                $"and pti.CustomerCode = ppi.CustomerCode\n" +
+                $"LEFT OUTER JOIN LabTransCompOrderInfo ltcoi\r\n" +
+                $"ON ltcoi.CompOrderDate = ppi.CompOrderDate\r\n" +
+                $"AND ltcoi.CompOrderNo = ppi.CompOrderNo\r\n" +
+                $"AND ltcoi.CompCode = ppi.CompCode\r\n" +
+                $"WHERE ppi.CustomerCode = 'banksalad'\r\n" +
+                $"{subQuery}" +
+                $"order by ppi.CompOrderDate ";
 
             var arrResponse = LabgeDatabase.SqlToJArray(sql);
             return Ok(arrResponse);
@@ -156,7 +156,7 @@ namespace supportsapi.labgenomics.com.Controllers.StrategyBusiness
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
